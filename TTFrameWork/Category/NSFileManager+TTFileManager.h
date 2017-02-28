19c4f93d39b7b4fe6d7a0d8629a6c98c1c030ae6
zhangfurun <122674287@qq.com>
2017-02-28 21:24:04 +0800

    1.TTFileManager的部分注解

--- TTFrameWork/Category/NSFileManager+TTFileManager.h
+++ TTFrameWork/Category/NSFileManager+TTFileManager.h
@@ -9,21 +9,43 @@
 #import <Foundation/Foundation.h>
 
 @interface NSFileManager (TTFileManager)
+
+/**
+ 判断是不是文件夹
+
+ @param path 判断对象的路径
+ */
 + (BOOL)isDirectoryExist:(NSString *)path;
+
+/**
+ 判断文件夹是不是存在
+
+ @param path 判断对象的路径
+ */
 + (BOOL)isFileExistAtPath:(NSString *)path;
+
 + (BOOL)isEmptyFolderWithPath:(NSString *)path error:(NSError **)error;
 + (BOOL)deleteFileAtPath:(NSString *)path isDirectory:(BOOL)isDir;
 + (BOOL)createDirectorysAtPath:(NSString *)path;
+
 /**
- * 获得文件夹大小
+ 获取文件大小
+
+ @param folderPath 文件夹path
  */
 + (long long)folderSizeAtPath:(NSString*)folderPath;
+
 /**
- * 获得单个文件大小
+  获得单个文件大小
+
+ @param filePath 单个文件的path
  */
 + (long long)fileSizeAtPath:(NSString*)filePath;
+
 /**
- * 转换大小单位
+ 转换大小单位
+
+ @param size 文件的大小
  */
 + (NSString *)sizeFormatByteCount:(long long)size;
 @end
