0090758e597481f8cc6a5d67d6f2689b62d5cca4
zhangfurun <122674287@qq.com>
2017-03-01 21:55:55 +0800

    1.完善TTString,添加集中获取唯一标识符的方法

--- TTFrameWork/Category/NSString+TTString.h
+++ TTFrameWork/Category/NSString+TTString.h
@@ -9,7 +9,36 @@
 #import <Foundation/Foundation.h>
 
 @interface NSString (TTString)
-- (BOOL)isNilOrEmpty; // 字符串判空
-- (NSString *)md5Str; // md5加密
-+ (NSString *)GUIDString; //
+
+/**
+ 字符串的判断
+ */
+- (BOOL)isNilOrEmpty;
+
+/**
+ 字符串的MD5加密
+ */
+- (NSString *)md5Str;
+
+/**
+ 生成唯一标识符(默认采用CFUUID,无限制)
+ */
++ (NSString *)GUIDString;
+
+/**
+ 通过NSUUID获取唯一标识符,限制:当前系统>=6.0
+ */
++ (NSString *)NSUUIDString;
+
+/**
+ Vindor标示符 
+ 不过获取这个IDFV的新方法被添加在已有的UIDevice类中.跟advertisingIdentifier一样，该方法返回的是一个NSUUID对象
+
+ */
++ (NSString *)vendorString;
+
+/**
+ 生成广告的唯一标识符
+ */
++ (NSString *)IDFAString;
 @end
--- TTFrameWork/Category/NSString+TTString.m
+++ TTFrameWork/Category/NSString+TTString.m
@@ -8,6 +8,9 @@
 
 #import "NSString+TTString.h"
 #import <CommonCrypto/CommonCrypto.h>
+#import <UIKit/UIKit.h>
+
+#import <AdSupport/ASIdentifierManager.h>
 
 @implementation NSString (TTString)
 - (BOOL)isNilOrEmpty {
@@ -32,11 +35,50 @@
     return [hash lowercaseString];
 }
 
+/**
+ 在2013年3月21日苹果已经通知开发者，从2013年5月1日起，访问UIDID的应用将不再能通过审核，替代的方案是开发者应该使用“在iOS 6中介绍的Vendor或Advertising标示符”。
+ 分散式系統中的所有元素，都能有唯一的辨识资讯，而不需要透过中央控制端來做辨识资讯的指定。如此一來，每個人都可以建立不与其它人冲突的 UUID。在這样的情況下，就不需考虑资料库建立时的名称重复问题
+ UUID OR GUID最大优点在於算法不依赖数据库，可以入库前预生成。
+ */
 + (NSString *)GUIDString {
-    CFUUIDRef uuidObj = CFUUIDCreate(nil);
-    NSString *uuidString = (NSString *)CFBridgingRelease(CFUUIDCreateString(nil, uuidObj));
-    uuidObj = nil;
-    return uuidString;
+    return [self UUIDString];
+}
+
++ (NSString *)NSUUIDString {
+    NSString *uuid = [[NSUUID UUID] UUIDString];
+    return uuid;
+}
+
+/**
+ 一个Vendor是CFBundleIdentifier（反转DNS格式）的前两部分
+ */
++ (NSString *)vendorString {
+    NSString *idfv = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    return idfv;
+}
+
+/**
+ UDID的全名为 Unique Device Identifier :设备唯一标识符
+ UDID是一个40位十六进制序列
+ 在iOS5 中可以获取,但是,在此之后,被苹果禁用了
+ + (NSString *)UDIDString {
+ NSString *udid = [[UIDevice currentDevice] uniqueIdentifier];
+ return udid;
+ }
+ */
+
++ (NSString *)UUIDString {
+    CFUUIDRef puuid = CFUUIDCreate( nil );
+    CFStringRef uuidString = CFUUIDCreateString( nil, puuid );
+    NSString * result = (NSString *)CFBridgingRelease(CFStringCreateCopy( NULL, uuidString));
+    CFRelease(puuid);
+    CFRelease(uuidString);
+    return result;
+}
+
++ (NSString *)IDFAString {
+    NSString *adId = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+    return adId;
 }
 
 @end
