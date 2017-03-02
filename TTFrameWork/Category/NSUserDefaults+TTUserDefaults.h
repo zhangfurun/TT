4bb4bbd9fc28fc9538c9f75bb652aeb98cf09838
zhangfurun <122674287@qq.com>
2017-03-02 22:31:46 +0800

    1.完善NSUserDefaults

--- TTFrameWork/Category/NSUserDefaults+TTUserDefaults.h
+++ TTFrameWork/Category/NSUserDefaults+TTUserDefaults.h
@@ -9,16 +9,99 @@
 #import <Foundation/Foundation.h>
 
 @interface NSUserDefaults (TTUserDefaults)
+#pragma mark - Set Data
+
+/**
+ 添加BOOL类型的数据
+
+ @param value 数据
+ @param defaultName 标志字符
+ */
 + (void)setBoolValue:(BOOL)value forKey:(NSString *)defaultName;
+
+/**
+ 添加Object类型的数据(任意常用类型,非自定义)
+ 
+ @param value 数据 // 注意,却别与setValue,这里的参数value不能为空
+ @param defauletName 标志字符
+ */
 + (void)setObjectValue:(id)value forKey:(NSString *)defauletName;
+
+/**
+ 添加任意常用数据
+
+ @param value 数据 Value参数可以为空
+ @param defauletName 标志字符
+ */
 + (void)setValue:(id)value forKey:(NSString *)defauletName;
+
+/**
+ 添加Integer类型的数据
+
+ @param value 数据
+ @param defaultName 标志字符
+ */
 + (void)setIntegerValue:(NSInteger)value forKey:(NSString *)defaultName;
-+ (BOOL)boolValueForKey:(NSString *)defaultName;
+
+
+#pragma mark - Get Data
+
+/**
+ 获取NSString类型的数据
+
+ @param defaultName 标志字符
+ */
 + (NSString *)stringValueForKey:(NSString *)defaultName;
+
+/**
+ 获取BOOL类型的数据
+
+ @param defaultName 标志字符
+ */
++ (BOOL)boolValueForKey:(NSString *)defaultName;
+
+/**
+ 获取NSInteger类型的数据
+
+ @param defaultName 标志字符
+ */
 + (NSInteger)integerValueForKey:(NSString *)defaultName;
+
+/**
+ 获取NSData类型的数据
+
+ @param defaultName 标志字符
+ */
 + (NSData *)dataValueForKey:(NSString *)defaultName;
-+ (BOOL)isExistsForKey:(NSString *)defaultName;
-+ (void)removeObjectForKey:(NSString *)defaultName;
+/**
+ 获取NSArray类型的数据
+ 
+ @param defaultName 标志字符
+ */
 + (NSArray *)arrayValueForKey:(NSString *)defaultName;
+/**
+ 获取NSMutableArray类型的数据
+ 
+ @param defaultName 标志字符
+ */
 + (NSMutableArray *)mutableArrayValueForKey:(NSString *)defaultName;
+
+#pragma mark - Delete Data
+
+/**
+ 删除数据
+
+ @param defaultName 需要删除的标志字符
+ */
++ (void)removeObjectForKey:(NSString *)defaultName;
+
+#pragma mark - Other
+
+/**
+ 查看本地保存数据中的所有标志字符是不是包括传入的参数
+ 即:判断是不是存在参数key
+
+ @param defaultName 需要判断的标志字符
+ */
++ (BOOL)isExistsForKey:(NSString *)defaultName;
 @end
--- TTFrameWork/Category/NSUserDefaults+TTUserDefaults.m
+++ TTFrameWork/Category/NSUserDefaults+TTUserDefaults.m
@@ -11,6 +11,9 @@
 #define USER_DEFAULTS [NSUserDefaults standardUserDefaults]
 
 @implementation NSUserDefaults (TTUserDefaults)
+
+#pragma mark - Set Data
+
 + (void)setBoolValue:(BOOL)value forKey:(NSString *)defaultName{
     [USER_DEFAULTS setBool:value forKey:defaultName];
     [USER_DEFAULTS synchronize];
@@ -31,16 +34,23 @@
     [USER_DEFAULTS synchronize];
 }
 
-+ (BOOL)boolValueForKey:(NSString *)defaultName{
-    return [USER_DEFAULTS boolForKey:defaultName];
-}
+
+#pragma mark - Get Data
 
 + (NSString *)stringValueForKey:(NSString *)defaultName{
     return [USER_DEFAULTS stringForKey:defaultName];
 }
 
++ (BOOL)boolValueForKey:(NSString *)defaultName{
+    return [USER_DEFAULTS boolForKey:defaultName];
+}
+
 + (NSInteger)integerValueForKey:(NSString *)defaultName{
     return [USER_DEFAULTS integerForKey:defaultName];
+}
+
++ (NSData *)dataValueForKey:(NSString *)defaultName {
+    return [USER_DEFAULTS dataForKey:defaultName];
 }
 
 + (NSArray *)arrayValueForKey:(NSString *)defaultName {
@@ -51,14 +61,8 @@
     return [USER_DEFAULTS mutableArrayValueForKey:defaultName];
 }
 
-+ (NSData *)dataValueForKey:(NSString *)defaultName {
-    return [USER_DEFAULTS dataForKey:defaultName];
-}
 
-+ (BOOL)isExistsForKey:(NSString *)defaultName{
-    NSArray *allKeys = USER_DEFAULTS.dictionaryRepresentation.allKeys;
-    return [allKeys containsObject:defaultName];
-}
+#pragma mark - Delete Data
 
 + (void)removeObjectForKey:(NSString *)defaultName {
     if ([self isExistsForKey:defaultName]) {
@@ -66,5 +70,14 @@
         [USER_DEFAULTS synchronize];
     }
 }
+
+
+#pragma mark - Other
+
++ (BOOL)isExistsForKey:(NSString *)defaultName{
+    NSArray *allKeys = USER_DEFAULTS.dictionaryRepresentation.allKeys;
+    return [allKeys containsObject:defaultName];
+}
+
 
 @end
