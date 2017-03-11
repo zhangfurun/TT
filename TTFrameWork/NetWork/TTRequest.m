c0f88a79f7cc638e231ecb9f8e4bd331d4fcbe7c
zhangfurun <122674287@qq.com>
2017-03-11 17:25:30 +0800

    1.优化TTRequest

--- TTFrameWork/NetWork/TTRequest.m
+++ TTFrameWork/NetWork/TTRequest.m
@@ -11,9 +11,9 @@
 #import "UIDevice+TTDevice.h"
 #import "NSBundle+TTBundle.h"
 
-NSString * const FORMAL_SERVER_URL = @"";
-NSString * const TEST_SERVER_URL = @"";
-NSString * const LOCAL_SERVER_URL = @"";
+NSString * const FORMAL_SERVER_URL = @""; //正式服务器
+NSString * const TEST_SERVER_URL = @""; //测试服务器
+NSString * const LOCAL_SERVER_URL = @""; //本地服务器(客户端方便与服务端)
 
 @implementation TTRequest
 - (NSString *)getRequestHost {
@@ -34,9 +34,18 @@ NSString * const LOCAL_SERVER_URL = @"";
     return agent;
 }
 
+// 针对数据读取的方便操作,重写isSuccess属性的get重写
 - (BOOL)isSuccess {
     return [self success];
 }
+
+/*
+ e.g.:
+ "status": {
+ "code": 0,
+ "msg": "string"
+ }
+ */
 
 - (BOOL)success {
     if ([self.resultDict.allKeys containsObject:@"status"]) {
@@ -54,6 +63,16 @@ NSString * const LOCAL_SERVER_URL = @"";
     }
     return @"";
 }
+/*
+ e.g.:
+ "page": {
+ "hasNext": true,
+ "pageCount": 0,
+ "pageNo": 0,
+ "pageSize": 0,
+ "rsCount": 0
+ }
+ */
 
 - (NSInteger)totalCount {
     return [self.resultDict[@"page"][@"rsCount"] integerValue];
