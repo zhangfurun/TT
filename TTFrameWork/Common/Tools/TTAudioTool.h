da47db7267eee23b42434e055ca1d50ce7b63356
zhangfurun <122674287@qq.com>
2017-03-21 20:46:06 +0800

    1.添加Tools

---
+++ TTFrameWork/Common/Tools/TTAudioTool.h
@@ -0,0 +1,13 @@
+//
+//  TTAudioTool.h
+//  TT
+//
+//  Created by 张福润 on 2017/3/21.
+//  Copyright © 2017年 张福润. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTAudioTool : NSObject
++ (float)fetchSystemVolume;
+@end
