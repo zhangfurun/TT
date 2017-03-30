e8675e9ceee8b9c8bd119e69b835fa096aebe5dd
zhangfurun <ifenghui@zhangfurundeiMac.local>
2017-03-30 19:38:38 +0800

    1.Update TTTextView
    add something about annotation

--- TTFrameWork/View/TTTextView/TTTextView.h
+++ TTFrameWork/View/TTTextView/TTTextView.h
@@ -9,14 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface TTTextView : UITextView
-
-/**
- 占位字符
- */
 @property (copy, nonatomic) NSString *placeHolerText;
-
-/**
- 占位字符的颜色
- */
 @property (strong, nonatomic) UIColor *placeHolerTextColor;
 @end
