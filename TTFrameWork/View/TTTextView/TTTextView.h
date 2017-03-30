f872af65761f59d1c399f30c9a59fb86a0cb86d6
zhangfurun <ifenghui@zhangfurundeiMac.local>
2017-03-30 18:48:08 +0800

    1.add change

--- TTFrameWork/View/TTTextView/TTTextView.h
+++ TTFrameWork/View/TTTextView/TTTextView.h
@@ -9,6 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface TTTextView : UITextView
+
+/**
+ 占位字符
+ */
 @property (copy, nonatomic) NSString *placeHolerText;
+
+/**
+ 占位字符的颜色
+ */
 @property (strong, nonatomic) UIColor *placeHolerTextColor;
 @end
